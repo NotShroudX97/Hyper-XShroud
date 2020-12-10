d99bcbd6042ef19d5d6003a84330d21f9d766d13
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2020-12-10 20:23:58 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -169,4 +169,3 @@ CMD_HELP.update({
     ".update\
 \nUsage: Checks if the main userbot repository has any updates and shows a changelog if so.\
 \n\n.update now\
-\nUsage: Updates your userbot, if there are any updates in the main userbot repository."
