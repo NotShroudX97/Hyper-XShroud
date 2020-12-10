00d92c44733f604590feceb2154f124ff8083a63
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2020-12-10 18:52:09 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,26 +1,15 @@
-import time
-from platform import python_version
+import asyncio
+from telethon import events
+from uniborg.util import admin_cmd
+from userbot import ALIVE_NAME
+from telethon.tl.types import ChannelParticipantsAdmins
 
-from telethon import version
+# uptime = get_readable_time((time.time() - Lastupdate))
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 
-from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from . import ALIVE_NAME, CMD_HELP, StartTime, catdef, catversion, mention, reply_id
-
-DEFAULTUSER = ALIVE_NAME or "cat"
-CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESFULLY ✮"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "  ✥ "
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-async def amireallyalive(alive):
-    if alive.fwd_from:
-        return
-    reply_to_id = await reply_id(alive)
-    uptime = await catdef.get_readable_time((time.time() - StartTime))
-    _, check_sgnirts = check_data_base_heal_th()
-    if  PM_IMG = "https://telegra.ph/file/c2c44df2e47dd6934ad75.jpg" 
+""" =======================CONSTANTS====================== """
+        PM_IMG = "https://telegra.ph/file/c2c44df2e47dd6934ad75.jpg" 
+""" =======================CONSTANTS====================== """
         pm_caption += pm_caption +=  f"➥       **//__↼🄼🄰🅂🅃🄴🅁⇀__//**      \n 『{DEFAULTUSER}』 \n"
         pm_caption += " \n\n"
         pm_caption += "✘ ΔβØỮŦ Μ¥ Ş¥ŞŦ€Μ ✘\n\n"
